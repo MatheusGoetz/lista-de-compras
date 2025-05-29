aa8e787e8909cac1634f1805419f73a990f4ca44
MatheusGoetz <goetz.dev@gamil.com>
2025-05-29 12:05:43 -0300

    capturando valor do input

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-let addInput = document.getElementById("product");
+const form = document.querySelector("form")
+const product = document.getElementById("product")
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    console.log(product.value)
+}
