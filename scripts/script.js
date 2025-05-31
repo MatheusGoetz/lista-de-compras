54e67404bf1d219a7fe2bed138b5cdac4b945245
Matheus Goetz <goetz.dev@gmail.com>
2025-05-31 10:00:09 -0300

    Adicionando item na lista de compras

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,49 @@
-let addInput = document.getElementById("product");
+const form = document.querySelector("form")
+const product = document.getElementById("product")
+const listItems = document.querySelector("ul")
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    const newItem = {
+      id: new Date().getTime(),
+      name: product.value
+    }
+    console.log(newItem.name)
+
+    if (newItem.name === "") {
+      alert("Por favor, informe um item para ser adicionado na lista.")
+    } else {
+       addProduct(newItem)
+    }
+}
+
+function addProduct(newItem){
+  try {
+    // Cria um novo item (li) na lista (ul).
+    const productItem = document.createElement("li")
+    productItem.classList.add("newItem")
+
+    // Cria input de checkbox.
+    const checkboxProduct = document.createElement("input")
+    checkboxProduct.type = "checkbox"
+
+    // Cria icone de remover.
+    const removeProduct = document.createElement("img")
+    removeProduct.classList.add("close")
+    removeProduct.setAttribute("src", "./assets/icon-delete.svg")
+    removeProduct.setAttribute("alt", "Icone de remoção")
+
+    // Cria o nome do produto.
+    const productName = document.createElement("div")
+    productName.classList.add("item")
+    productName.textContent = newItem.name
+
+    productItem.append(checkboxProduct, productName, removeProduct)
+    
+    listItems.append(productItem)
+
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possivel adicionar um novo item")
+  }
+}
